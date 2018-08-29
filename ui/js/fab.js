a4a1361320ddc391d9da0eaea083ce01a9975b7c
Daniel <dan4allu93@gmail.com>
2018-08-29 20:17:22 +0100

    added responsiveness to the pages

--- ui/js/fab.js
+++ ui/js/fab.js
@@ -1,10 +1,12 @@
+"use strict";
+
 var menu = document.querySelector(".js-fab__menu");
 var button = document.querySelector(".js-fab");
 
-toggleMenu = function() {
+function toggleMenu() {
   button.classList.replace("fa-shopping-cart", "fa-times");
   menu.classList.toggle("open");
-};
+}
 
 button.addEventListener("click", function() {
   toggleMenu();
