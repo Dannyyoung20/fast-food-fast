a618d0ed7c1f130af3b2db1ed0a3f1130bbeb5ec
Daniel <dan4allu93@gmail.com>
2018-09-27 13:11:24 +0100

    commit adds comment on the generate fn

--- db/index.js
+++ db/index.js
@@ -3,6 +3,8 @@ import config from '../config';
 
 const pool = new Pool(config.dbOptions);
 
+// Fn used to get all items in angiven table
+// @params table TABLE_NAME, cb Callback fn
 function selectAll(table, cb) {
   const query = `SELECT * FROM ${table}`;
 
@@ -15,6 +17,8 @@ function selectAll(table, cb) {
   });
 }
 
+// Fn used to select a particular item in the db based on id
+// @params table TABLE_NAME, id OBEJECT_ID, cb Callback fn
 function selectById(table, id, cb) {
   const query = `SELECT * FROM ${table} WHERE id=$1`;
 
@@ -27,6 +31,8 @@ function selectById(table, id, cb) {
   });
 }
 
+// Fn used to get user via email and password
+// @params table TABLE_NAME, credentials [email, password], cb Callback fn
 function selectByEmailPassword(table, credentials, cb) {
   const query = `SELECT * FROM ${table} WHERE email=$1 AND password=$2`;
 
@@ -39,6 +45,8 @@ function selectByEmailPassword(table, credentials, cb) {
   });
 }
 
+// Fn used to update a particaular record based on the id
+// @params query SQL_QUERY, id OBEJECT_ID, cb Callback fn
 function updateById(query, id, cb) {
   pool.connect((err, db, done) => {
     db.query(query, [id], (error, response) => {
@@ -49,6 +57,8 @@ function updateById(query, id, cb) {
   });
 }
 
+// Fn used to insert a record into a given table
+// @params query SQL_QUERY, credentials [], cb Callback fn
 function insert(query, credentials, cb) {
   pool.connect((err, db, done) => {
     db.query(query, credentials, (error, response) => {
