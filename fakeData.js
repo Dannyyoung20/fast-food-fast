8eb4564da61bae90771e8decc04cd065818a4a1d
Daniel <dan4allu93@gmail.com>
2018-09-28 01:22:15 +0100

    commit create test cases for store.js

---
+++ fakeData.js
@@ -0,0 +1,16 @@
+export default {
+  user: {
+    name: 'Eze Daniel',
+    address: 'Somewhere in lagos',
+    email: 'dan4allu93@gmail.com',
+    password: 'password',
+  },
+  order: {
+    id: 1,
+    item: 'pizza',
+    price: 3000,
+    size: 'large',
+    quantity: 4,
+    toppings: null,
+  },
+};
