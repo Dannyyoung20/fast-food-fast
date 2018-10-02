838d32a88e25648816b4a6a8a2b5af044e01e525
Daniel <dan4allu93@gmail.com>
2018-10-02 10:24:15 +0100

    commit fixes the set headers bug

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,7 +15,6 @@ class User {
     const decoded = Authorization.JWT_VERIFY(token);
     const { id } = decoded.user;
     const query = `SELECT * FROM orders WHERE id = ${id}`;
-
     pool.query(query)
       .then((result) => {
         if (result.rowCount === 0) return res.status(200).json({ message: SUCCESSFUL_REQUEST_MSG, history: 'No order history' });
